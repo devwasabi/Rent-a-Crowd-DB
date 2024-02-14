34f499d9600136ac2d31f76c60b0ad7f0e6cd6c7
Vutlhari Mashimbyi <global-vutlhari.mashimbyi@bbd,ac.za>
2024-02-14 20:01:39 +0200

    Fixed procedures

--- schema-model/Funtions/dbo.ValidatePhoneNumber.sql
+++ schema-model/Funtions/dbo.ValidatePhoneNumber.sql
@@ -1,3 +1,6 @@
+--DROP FUNCTION IF EXISTS ValidatePhoneNumber;
+
+
 CREATE FUNCTION dbo.ValidatePhoneNumber (
     @PhoneNumber VARCHAR(10)
 )
@@ -5,9 +8,11 @@ RETURNS BIT
 AS
 BEGIN
     DECLARE @IsValid BIT = 0;
+  -- Check length is 10 digits
+	IF @PhoneNumber LIKE '[0-0][6-8][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' AND LEN(@PhoneNumber) = 10
+    BEGIN
+	SET @IsValid = 1
+	END
 
-    IF @PhoneNumber LIKE '0[6-8][0-9]{8}'
-        SET @IsValid = 1;
-
-    RETURN @IsValid;
+  RETURN @isValid;
 END;
