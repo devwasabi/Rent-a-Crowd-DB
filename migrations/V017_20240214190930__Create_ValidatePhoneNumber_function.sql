77c170479ee73eb7d583266a82baf5383b0f848b
Lesedi Jonas <lesedij@bbd.co.za>
2024-02-14 21:12:54 +0200

    added functions

---
+++ migrations/V017_20240214190930__Create_ValidatePhoneNumber_function.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION dbo.ValidatePhoneNumber (
+    @PhoneNumber VARCHAR(10)
+)
+RETURNS BIT
+AS
+BEGIN
+  DECLARE @IsValid BIT = 0;
+	
+    IF @PhoneNumber LIKE '[0-0][6-8][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' AND LEN(@PhoneNumber) = 10
+    BEGIN
+        SET @IsValid = 1
+    END
+
+  RETURN @isValid;
+END;
