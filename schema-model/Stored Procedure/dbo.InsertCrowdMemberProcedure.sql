33527438dc1ad9256a2a0099e82e9d4238b3df99
Vutlhari Mashimbyi <global-vutlhari.mashimbyi@bbd,ac.za>
2024-02-14 13:28:44 +0200

    Fixed crowdmember procedure

--- schema-model/Stored Procedure/dbo.InsertCrowdMemberProcedure.sql
+++ schema-model/Stored Procedure/dbo.InsertCrowdMemberProcedure.sql
@@ -2,27 +2,15 @@ CREATE PROCEDURE InsertCrowdMemberProcedure
     @Email VARCHAR(255)
 AS
 BEGIN
-    DECLARE @AgeGroup VARCHAR(10);
-    DECLARE @Rate INT;
+
     DECLARE @UserID INT;
 
-    -- Get AgeGroup and UserID from UserInfo table based on the user's email
-    SELECT @AgeGroup = ageGroup, @UserID = userId
+    -- UserID from UserInfo table based on the user's email
+    SELECT @UserID = userId
     FROM UserInfo
     WHERE Email = @Email;
 
-    -- Determine rate based on AgeGroup
-    IF @AgeGroup = 'toddler'
-        SET @Rate = 20;
-    ELSE IF @AgeGroup = 'youth'
-        SET @Rate = 30;
-    ELSE IF @AgeGroup = 'adult'
-        SET @Rate = 40;
-    ELSE
-        -- Handle cases where AgeGroup doesn't match any known value
-        SET @Rate = 0; 
-
     -- Insert record into CrowdMembers table
-    INSERT INTO CrowdMembers (userId, rate)
-    VALUES (@UserID, @Rate);
+    INSERT INTO CrowdMembers (userId)
+    VALUES (@UserID);
 END;
