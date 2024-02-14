70d74c1fef42d2cb8b002c2bd06c96a6f0c1152e
Vutlhari-Mashimbyi <159114894+Vutlhari-Mashimbyi@users.noreply.github.com>
2024-02-14 20:21:32 +0200

    Fixed procedures (#7)
    Co-authored-by: Vutlhari Mashimbyi <global-vutlhari.mashimbyi@bbd,ac.za>

--- schema-model/Stored Procedure/Examples/InsertEventProcedure_Example.sql
+++ schema-model/Stored Procedure/Examples/InsertEventProcedure_Example.sql
@@ -1,3 +1,7 @@
-EXEC InsertEventProcedure 'vutlhari@bbd.com', 3, 'all', 'b', '2024-05-15', '13:00:00';
+--INSERT INTO Rates (rate,startDate) VALUES (50,'2024-02-23');
+--INSERT INTO Commissions (commission,startDate) VALUES (0.5,'2024-02-23')
+
+
+EXEC InsertEventProcedure 'vutlhari@bbd.com', 3,  '2024-05-15 13:00:00';
 
 --Note the payable and event ID to make a payment
