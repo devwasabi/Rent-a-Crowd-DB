9416f032ae5cd0ef72b03d05479b04050f5b317e
Lesedi Jonas <lesedij@bbd.co.za>
2024-02-15 15:43:09 +0200

    Added views migrations

---
+++ migrations/V018_20240215133658__Create_AttendanceReporting_view.sql
@@ -0,0 +1,13 @@
+--See the users who are attending your event
+
+CREATE VIEW [dbo].[AttendanceReportingView]
+AS
+    SELECT u.firstName AS FirstName,
+           u.lastName AS LastName,
+           u.email AS Email,
+           u.phone AS PhoneNumber,
+           e.eventName AS EventName,
+		   b.eventId AS eventId
+    FROM Bookings b
+    INNER JOIN UserInfo u ON b.crowdMemberId = u.userId
+	INNER JOIN Events e ON b.eventId = e.eventId;
